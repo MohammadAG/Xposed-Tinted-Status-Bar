e8d57c07a0645468f882a3bb21562bc05d0ebac6
GermainZ <germanosz@gmail.com>
2014-02-19 06:09:05 +0200

    Add search to activities list

--- src/com/mohammadag/colouredstatusbar/ActivitesListActivity.java
+++ src/com/mohammadag/colouredstatusbar/ActivitesListActivity.java
@@ -1,8 +1,11 @@
 package com.mohammadag.colouredstatusbar;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 
+import android.annotation.SuppressLint;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -17,7 +20,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,6 +37,13 @@ public class ActivitesListActivity extends ListActivity {
 	private Switch mSwitch;
 	private SettingsHelper mSettingsHelper;
 	protected boolean mDirty;
+
+	private ArrayList<String> mActivityList = new ArrayList<String>();
+	private ArrayList<String> mFilteredActivityList = new ArrayList<String>();
+
+	private MenuItem mSearchItem;
+	private ActivityListAdapter mActivityListAdapter;
+	private String mNameFilter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +63,6 @@ public class ActivitesListActivity extends ListActivity {
 
 		loadActivitesForPackage(mPackageName);
 
-
 		float scale = getResources().getDisplayMetrics().density;
 		int padding = (int) (8 * scale + 0.5f);
 		getListView().setPadding(padding * 2, padding, padding * 2, padding);
@@ -63,6 +75,40 @@ public class ActivitesListActivity extends ListActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activites_list, menu);
+
+		mSearchItem = menu.findItem(R.id.action_search);
+		final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+
+		mSearchItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+			@Override
+			public boolean onMenuItemActionExpand(MenuItem menuItem) {
+				searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+					@Override
+					public boolean onQueryTextSubmit(String query) {
+						mNameFilter = query;
+						mActivityListAdapter.getFilter().filter(mNameFilter);
+						findViewById(R.id.action_search).clearFocus();
+						return false;
+					}
+
+					@Override
+					public boolean onQueryTextChange(String newText) {
+						mNameFilter = newText;
+						mActivityListAdapter.getFilter().filter(mNameFilter);
+						return false;
+					}
+
+				});
+				return true;
+			}
+
+			@Override
+			public boolean onMenuItemActionCollapse(MenuItem menuItem) {
+				mActivityListAdapter.getFilter().filter("");
+				return true;
+			}
+		});
 
 		MenuItem actionSwitch = menu.findItem(R.id.switch_button);
 		mSwitch = (Switch) actionSwitch.getActionView().findViewById(R.id.color_switch);
@@ -85,27 +131,34 @@ public class ActivitesListActivity extends ListActivity {
 	}
 
 	@Override
+	public boolean onSearchRequested() {
+		mSearchItem.expandActionView();
+		return super.onSearchRequested();
+	}
+
+	@Override
 	protected void onListItemClick(ListView list, View view, int position, long id) {
 		Intent i = new Intent(getApplicationContext(), ApplicationSettings.class);
 		i.putExtra(Common.EXTRA_KEY_PACKAGE_NAME, mPackageName);
 		i.putExtra(Common.EXTRA_KEY_PACKAGE_FRIENDLY_NAME, mFriendlyPackageName);
 		String activityName = "NONE";
-		if (position != 0)
-			activityName = Utils.removePackageName(
-					((TextView)view.findViewById(android.R.id.text1)).getText().toString(), mPackageName);
+		String activityNameTemp = ((TextView)view.findViewById(android.R.id.text1)).getText().toString();
+		if (activityNameTemp != getString(R.string.all_the_wonderful_activities))
+			activityName = Utils.removePackageName(activityNameTemp, mPackageName);
 		i.putExtra(Common.EXTRA_KEY_ACTIVITY_NAME, activityName);
 		startActivityForResult(i, position);
 	}
 
 	private void loadActivitesForPackage(String packageName) {
 		try {
+			mActivityList.clear();
+
 			PackageManager pm = getPackageManager();
 			PackageInfo info = pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
 			ActivityInfo[] list = info.activities;
 			getActionBar().setIcon(info.applicationInfo.loadIcon(pm));
 
-			List<String> activityNames = new ArrayList<String>(); 
-			activityNames.add(getString(R.string.all_the_wonderful_activities));
+			mActivityList.add(getString(R.string.all_the_wonderful_activities));
 
 			if (list == null) {
 				Toast.makeText(this, R.string.no_activities_error, Toast.LENGTH_SHORT).show();
@@ -117,51 +170,117 @@ public class ActivitesListActivity extends ListActivity {
 			if (packageName.equals(Common.PACKAGE_NAME_GOOGLE_SEARCH)) {
 				/* Version code from APK on the intenret */
 				if (info.versionCode >= Common.GOOGLE_SEARCH_VERSION_CODE_WITH_GEL) {
-					if (!activityNames.contains(Common.GEL_ACTIVITY_NAME)) {
-						activityNames.add(Common.GEL_ACTIVITY_NAME);
+					if (!mActivityList.contains(Common.GEL_ACTIVITY_NAME)) {
+						mActivityList.add(Common.GEL_ACTIVITY_NAME);
 					}
 				}
 			}
 
 			for (int i = 0; i < list.length; i++) {
-				activityNames.add(list[i].name);
+				mActivityList.add(list[i].name);
 			}
 
 			if (packageName.equals("com.google.android.launcher")) {
 				Toast.makeText(this, R.string.gel_stub_toast, Toast.LENGTH_LONG).show();
 			}
 
-			ArrayAdapter<String> activityListAdapter = new ArrayAdapter<String>(activityNames) {
-				@Override
-				public View getView(int position, View convertView, ViewGroup parent) {
-					// Load or reuse the view for this row
-					View row = convertView;
-					if (row == null) {
-						row = getLayoutInflater().inflate(android.R.layout.simple_list_item_1, parent, false);
-					}
+			mActivityListAdapter = new ActivityListAdapter(mActivityList);
 
-					String activityName = getItem(position);
-
-					TextView textView = (TextView) row.findViewById(android.R.id.text1);
-					textView.setText(activityName);
-					boolean isEnabled = 
-							mSettingsHelper.isEnabled(mPackageName, Utils.removePackageName(activityName, mPackageName));
-
-					textView.setTextColor(isEnabled ? Color.WHITE : Color.RED);
-
-					if (position == 0) {
-						textView.setTypeface(null, Typeface.BOLD);
-					}
-
-					return row;
-				}
-			};
-			AlphaInAnimationAdapter alphaAdapter = new AlphaInAnimationAdapter(activityListAdapter);
+			AlphaInAnimationAdapter alphaAdapter = new AlphaInAnimationAdapter(mActivityListAdapter);
 			getListView().setAdapter(alphaAdapter);
 			alphaAdapter.setAbsListView(getListView());
 		} catch (NameNotFoundException e) {
 			e.printStackTrace();
 			finish();
+		}
+	}
+
+	class ActivityListAdapter extends ArrayAdapter<String> implements Filterable {
+
+		private Filter filter;
+
+		public ActivityListAdapter(List<String> items) {
+			super(new ArrayList<String>(items));
+			mFilteredActivityList.addAll(items);
+			filter = new ActivityListFilter(this);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// Load or reuse the view for this row
+			View row = convertView;
+			if (row == null) {
+				row = getLayoutInflater().inflate(android.R.layout.simple_list_item_1, parent, false);
+			}
+
+			String activityName = mFilteredActivityList.get(position);
+
+			TextView textView = (TextView) row.findViewById(android.R.id.text1);
+			textView.setText(activityName);
+			boolean isEnabled =
+					mSettingsHelper.isEnabled(mPackageName, Utils.removePackageName(activityName, mPackageName));
+
+			textView.setTextColor(isEnabled ? Color.WHITE : Color.RED);
+
+			if (position == 0) {
+				textView.setTypeface(null, Typeface.BOLD);
+			}
+
+			return row;
+		}
+
+		@Override
+		public Filter getFilter() {
+			return filter;
+		}
+
+	}
+
+	private class ActivityListFilter extends Filter {
+
+		private ActivityListAdapter adaptor;
+
+		ActivityListFilter(ActivityListAdapter adaptor) {
+			super();
+			this.adaptor = adaptor;
+		}
+
+		@SuppressLint("WorldReadableFiles")
+		@Override
+		protected FilterResults performFiltering(CharSequence constraint) {
+			// NOTE: this function is *always* called from a background thread, and
+			// not the UI thread.
+
+			ArrayList<String> items = new ArrayList<String>();
+			synchronized (this) {
+				items.addAll(mActivityList);
+			}
+
+			FilterResults result = new FilterResults();
+			if (constraint != null && constraint.length() > 0) {
+				Pattern regexp = Pattern.compile(constraint.toString(), Pattern.LITERAL | Pattern.CASE_INSENSITIVE);
+				for (Iterator<String> i = items.iterator(); i.hasNext(); ) {
+					String name = i.next();
+					if (!regexp.matcher(name == null ? "" : name).find()) {
+						i.remove();
+					}
+				}
+			}
+
+			result.values = items;
+			result.count = items.size();
+
+			return result;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		protected void publishResults(CharSequence constraint, Filter.FilterResults results) {
+			// NOTE: this function is *always* called from the UI thread.
+			mFilteredActivityList = (ArrayList<String>) results.values;
+			adaptor.clear();
+			adaptor.addAll(mFilteredActivityList);
+			adaptor.notifyDataSetInvalidated();
 		}
 	}
 
