6f28cb29deb96430f4229a3a7ae78246be2bb67e
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2014-03-24 13:22:29 +0200

    Lints, lints, lints

--- src/com/mohammadag/colouredstatusbar/hooks/HtcTransparencyHook.java
+++ src/com/mohammadag/colouredstatusbar/hooks/HtcTransparencyHook.java
@@ -1,11 +1,13 @@
 package com.mohammadag.colouredstatusbar.hooks;
 
+import java.util.Locale;
+
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 
 public class HtcTransparencyHook {
 	public static void doHook(ClassLoader classLoader) {
-		if (!android.os.Build.MANUFACTURER.toLowerCase().contains("htc"))
+		if (!android.os.Build.MANUFACTURER.toLowerCase(Locale.getDefault()).contains("htc"))
 			return;
 
 		XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar", classLoader,
