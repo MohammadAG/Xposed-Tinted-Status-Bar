ec5329a45e34b277f591ad5bdc7da7864d1d7009
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2014-06-12 15:02:29 +0300

    Fix formatting

--- src/com/mohammadag/colouredstatusbar/preferences/MultiLineCheckBoxPreference.java
+++ src/com/mohammadag/colouredstatusbar/preferences/MultiLineCheckBoxPreference.java
@@ -7,33 +7,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MultiLineCheckBoxPreference extends CheckBoxPreference{
+public class MultiLineCheckBoxPreference extends CheckBoxPreference {
 
 	public MultiLineCheckBoxPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	protected void onBindView( View view){
-	    super.onBindView(view);
-	    makeMultiline(view);  	 
+	protected void onBindView(View view) {
+		super.onBindView(view);
+		makeMultiline(view);
 	}
 
-	 protected void makeMultiline( View view) {
-		 if (view instanceof ViewGroup)
-		 {
-			 ViewGroup viewGroup=(ViewGroup)view;
+	protected void makeMultiline(View view) {
+		if (view instanceof ViewGroup) {
+			ViewGroup viewGroup = (ViewGroup) view;
 
-			 for (int index = 0; index < viewGroup.getChildCount(); index++)
-			 {
-				 makeMultiline(viewGroup.getChildAt(index));
-			 }
-		 } else if (view instanceof TextView)
-		 {
-		        TextView t = (TextView)view;
-		        t.setSingleLine(false); 
-		        t.setEllipsize(null);
-		 }
+			for (int index = 0; index < viewGroup.getChildCount(); index++) {
+				makeMultiline(viewGroup.getChildAt(index));
+			}
+		} else if (view instanceof TextView) {
+			TextView t = (TextView) view;
+			t.setSingleLine(false);
+			t.setEllipsize(null);
+		}
 	}
 }
